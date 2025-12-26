c8ffe502d0686201af3c0f9b594ed8178afdc136
Irsyad <mr.icadj@gmail.com>
2025-12-26 07:46:53 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
-import { getFirestore } "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
